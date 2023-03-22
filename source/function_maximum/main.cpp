b445b2e5ae89758c7622806ac196f781f1a6ad15
blin04 <ivanbokor17@gmail.com>
2023-03-22 17:45:10 +0100

    implemented function that initializes population

--- source/function_maximum/main.cpp
+++ source/function_maximum/main.cpp
@@ -2,20 +2,28 @@
 #include "organism.h"
 using namespace std;
 
-// target function
+// target function, defined on [A, B]
 double f(double x) {
-    return 1.0;
+    return (2*sin(x)*sin(x)) / x;
 }
 
 // global parameters - maybe put into a separate file?
 const int POP_SIZE = 100;
-const double A, B;  // interval on which the maximum is searched
+const double A = -10.0;     // interval start
+const double B = 10.0;      // interval end 
+const int N = 32;           // length of chromosome
 vector<Organism> POPULATION;
 
 // fucntions
 double cost(const Organism &o) {
     // calculate how good an organism is
     return 1.0;
+}
+
+double getEncodedNumber(Organism o)
+{
+    double x = (o.getValue()) / ((1 << (N - 1)) - 1.0);
+    return A + x*(B - A);
 }
 
 int createRandomDNA() {
@@ -29,7 +37,7 @@ int createRandomDNA() {
         }
         // else bit stays 0
     }
-
+    
     return DNA;
 }
 
@@ -48,6 +56,8 @@ void initializePopulation() {
 int main() {
 
     initializePopulation(); 
+
+    cout << getEncodedNumber(POPULATION[0]) << "\n";
 
     return 0;
 }
